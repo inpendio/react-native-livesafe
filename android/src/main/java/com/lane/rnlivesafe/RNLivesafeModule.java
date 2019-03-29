4470ce8c5ded5f5a2a7fc0ac6171b1ff465f3ed4
danil fnf <danil@flownform.com>
2019-03-29 10:45:52 +0100

    typo

--- android/src/main/java/com/lane/rnlivesafe/RNLivesafeModule.java
+++ android/src/main/java/com/lane/rnlivesafe/RNLivesafeModule.java
@@ -138,7 +138,7 @@ public class RNLivesafeModule extends ReactContextBaseJavaModule {
                 tipTypeObject.getInt("value"),
                 tipTypeObject.getString("name"),
                 tipTypeObject.hasKey("icon") ? tipTypeObject.getString("icon") : "",
-                tipTypeObject.hasKey("mapIcon") ? tipTypeObject.getString("mapICon"): "",
+                tipTypeObject.hasKey("mapIcon") ? tipTypeObject.getString("mapIcon"): "",
                 tipTypeObject.hasKey("hintText") ? tipTypeObject.getString("hintText") : "",
                 tipTypeObject.hasKey("chatText") ? tipTypeObject.getString("chatText") : ""
         );
